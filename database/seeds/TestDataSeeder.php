279b0c87e64612b7f796e32a0a8010219f9400dc
watanabe-m-e-ize <m.watanabe@e-ize.jp>
2020-08-20 02:08:01 +0000

    Seeder編集

--- database/seeds/TestDataSeeder.php
+++ database/seeds/TestDataSeeder.php
@@ -12,17 +12,25 @@ class TestDataSeeder extends Seeder
     public function run()
     {
         // customersテーブルに登録するレコードを配列で定義する。
-        DB::table('customers')->insert([
-            [
-                'name'           => '太郎くん',
-                'created_at'     => now(),
-                'updated_at'     => now(),
-            ],
-            [
-                'name'           => '次郎くん',
-                'created_at'     => now(),
-                'updated_at'     => now(),
-            ],
-        ]);
+        // DB::table('customers')->insert([
+        //     [
+        //         'name'           => '太郎くん',
+        //         'created_at'     => now(),
+        //         'updated_at'     => now(),
+        //     ],
+        //     [
+        //         'name'           => '次郎くん',
+        //         'created_at'     => now(),
+        //         'updated_at'     => now(),
+        //     ],
+        // ]);
+        
+        // factoryを用いてcustomersにレコードを２件、
+        // さらにcustomer_idに紐づくreportsをそれぞれ登録する。
+        factory(App\Customer::class, 2)
+           ->create()
+           ->each(function ($customer) {
+                $customer->reports()->save(factory(App\Report::class)->make());
+            });
     }
 }
