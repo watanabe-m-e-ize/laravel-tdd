1f3c01104a48c318f06cca6d12e3f15e9b16263a
watanabe-m-e-ize <m.watanabe@e-ize.jp>
2020-08-20 01:53:36 +0000

    テストコード追加:[api/customers]にGETメソッドでアクセスできる

---
+++ tests/Feature/CustomerControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CustomerControllerTest extends TestCase
+{
+    public function testIndex()
+    {
+        $response = $this->get('/api/customers');
+
+        $response->assertStatus(200);
+    }
+}
