70c5c9fabf7d5667001300a7172cdd9ddc79a76a
watanabe-m-e-ize <m.watanabe@e-ize.jp>
2020-08-20 02:10:57 +0000

    Model編集

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    // Reportモデルを子に持つことを記述する
+    public function reports()
+    {
+    return $this->hasOne('App\Report');
+    }
 }
--- app/Report.php
+++ app/Report.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    //
+    // Customerモデルを親に持つことを記述する
+    public function customers()
+    {
+    return $this->belongTo('App\Customer');
+    }
 }
